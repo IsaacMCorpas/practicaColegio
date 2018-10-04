1cb038adfa4192d780918de46025ab24b9da9723
IsaacMCorpas <IsaacMCorpas@users.noreply.github.com>
2018-10-04 19:51:29 +0200

    cambios

---
+++ RegistroColegio/pruebawordpress/wp-content/themes/hiero/tpl-colegio.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Template Name: Registro Colegio
+*/
+
+
+
+get_header();
+
+$nombre = $_POST['nombre'];
+$nif = $_POST['nif'];
+$direccion = $_POST['direccion'];
+$email = $_POST['email'];
+$web = $_POST['web'];
+$telefono = $_POST['telefono'];
+$licencia = $_POST['licencia'];
+
+
+/*SQL Consulta */
+
+$sql = "INSERT INTO fcolegio (nombre,nif,direccion,email,web,telefono,licencia) VALUES 
+('".$nombre."','".$nif."','".$direccion."','".$email."','".$web."','".$telefono."','".$licencia."');";
+
+/**/
+
+$wpdb->query($sql);
+
+?>
+
+
+	<div id="primary">
+		<div id="content" role="main">
+            <div id="center">
+            <h2>Formulario del Colegio</h2>
+            <form method="post" enctype="multipart/form-data"
+                  action="<?php echo get_home_url(); ?>/?page_id=7">
+
+
+                <label>Nombre:</label>
+                <input type="text" name="nombre">
+                <br><br>
+
+                <label>NIF:</label>
+                <input type="text" name="nif">
+                <br><br>
+
+                <label>Direccion:</label>
+                <input type="text" name="direccion">
+                <br><br>
+
+                <label>Email:</label>
+                <input type="text" name="email">
+                <br><br>
+
+                <label>Web:</label>
+                <input type="text" name="web">
+                <br><br>
+
+                <label>Telefono:</label>
+                <input type="text" name="telefono">
+                <br><br>
+
+                <label>Licencia:</label>
+                <input type="text" name="licencia">
+                <br><br>
+
+                <input type="submit" value="Enviar">
+
+
+            </form>
+		</div><!-- #main -->
+	</div><!-- #primary -->
+</div>
+
+
+<?php get_footer();
